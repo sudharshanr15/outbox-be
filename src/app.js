877a4c644933a292592a54b0b1113228ebe46176
Sudharshan R <sudharshanrms2004@gmail.com>
2025-07-24 15:06:52 +0530

    Installed cors middleware

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var dotenv = require("dotenv")
+var cors = require("cors")
 dotenv.config()
 
 var indexRouter = require('./routes/index');
@@ -11,6 +12,7 @@ var mailRouter = require("./routes/mail")
 
 var app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
